2dc3df38733df968aeda56db41dbed4430c75f68
George <g3orrg301@gmail.com>
2023-06-12 21:46:15 +0300

    Create task for Monstri

--- resume/src/app/page.js
+++ resume/src/app/page.js
@@ -9,6 +9,16 @@ export default async function Home() {
   console.log(12234);
   console.log(projects)
 
+  // acuma Vcitore ai dat de dracu
+  // trebuie sa randezi 2 proiecte pe site
+  // am creeat variabila "projects" in care vei gasi structura a 2 proiecte din DB
+  // tu vei itera peste aceasta variabila si o sa te folosesti de componenta ProfileCard ca sa afisezi proiectul
+  // incearca sa nu te folosesti de chat GPT
+  // bafta
+
+  // variabila "projects" o mai poti vedea logata in terminal, dupa ce iti pornesti serverul
+
+
   return (
     <>
       <header>Header</header>
