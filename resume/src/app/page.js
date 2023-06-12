3ea0452f296edc87c4874e97dd6a4548e5227c4d
George <g3orrg301@gmail.com>
2023-06-12 23:59:15 +0300

    Add task for Victorashu

--- resume/src/app/page.js
+++ resume/src/app/page.js
@@ -10,14 +10,22 @@ export default async function Home() {
   console.log(projects)
 
   // acuma Vcitore ai dat de dracu
-  // trebuie sa randezi 2 proiecte pe site
-  // am creeat variabila "projects" in care vei gasi structura a 2 proiecte din DB
-  // tu vei itera peste aceasta variabila si o sa te folosesti de componenta ProfileCard ca sa afisezi proiectul
+  // trebuie sa randezi proiectele pe site ca si cum ar veni din DB
+  // am creeat variabila "projects" in care vei gasi structura proiectelor din DB
+  // te vei folosi de aceasta variabila ca sa afisezi proiectele
   // incearca sa nu te folosesti de chat GPT
   // bafta
 
-  // variabila "projects" o mai poti vedea logata in terminal, dupa ce iti pornesti serverul
+  // Variabila "projects" o mai poti vedea logata in terminal, dupa ce iti pornesti serverul (linia 10 de mai sus)
+  // nu uita sa faci commituri (quick-save practic) dupa ce ternimi o functionalitate
 
+
+
+  // Problema nr 2:
+  // Daca termni repede, poti sa creezi o componenta-parinte
+  // care sa cuprinda lista de proiecte pe care ai generat-o la primul punct, mai sus
+  // Si sa mai curinda un h3 descriptiv
+  // bafta
 
   return (
     <>
