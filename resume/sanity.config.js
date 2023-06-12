95087a629c17bfc96eb997ec8ec69f2da67c088a
George <g3orrg301@gmail.com>
2023-06-12 19:50:09 +0300

    Added and tested Sanity

--- resume/sanity.config.js
+++ resume/sanity.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
+import schemas from './sanity/schemas';
 
 const config = defineConfig({
     projectId: "zt1oa1hr",
@@ -8,6 +9,7 @@ const config = defineConfig({
     apiVersion: "2023-06-12",
     basePath: "/admin",
     plugins: [deskTool()],
+    schema: { types: [schemas] },
 });
 
 export default config;
--- resume/src/app/page.js
+++ resume/src/app/page.js
@@ -1,6 +1,13 @@
+import { getProjects } from "../../sanity/sanity-utils";
 import Button from "./components/atoms/Button";
+import { projects } from './helpers'
 
-export default function Home() {
+export default async function Home() {
+  // NU DECOMMENTA URMATOAREA LINIE PLS
+  // const projects = await getProjects();
+  console.log(12234);
+  console.log(projects)
+
   return (
     <>
       <header>Header</header>
