f9ccdb1d86ef1ddcb5626c2722f3af6cbd4e0c2c
George <g3orrg301@gmail.com>
2023-06-14 16:17:23 +0300

    Research and implement CRUD

--- personalSite/backend/index.js
+++ personalSite/backend/index.js
@@ -2,14 +2,21 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-//   next();
-// })
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+})
+
+app.use(express.json())
 
 app.get('/api', (req, res) => {
   res.json({id:1, name: 'cristian diaconecsu'})
+})
+
+app.post('/api/posts', (req, res) => {
+  console.log(req.body)
+  res.json({thanks: 'nicolae'})
 })
 
 app.listen(port, () => {
--- personalSite/frontend/src/App.jsx
+++ personalSite/frontend/src/App.jsx
@@ -5,21 +5,31 @@ import Button from './components/atoms/Button';
 import ProfileCard from "./components/molecules/ProfileCard";
 import { projects } from './helpers'
 
-
 function App() {
-  const [myData, setMyData] = useState([]);
-  useEffect(() => {
+  const postare = () => {
+    console.log('iata clickul')
 
+    fetch('http://localhost:8080/api/posts', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({name: 'kyle'})
+    }).then(r => r.json()).then(r => console.log(r))
+  }
+
+  useEffect(() => {
     fetch('http://localhost:8080/api')
       .then(r => r.json())
       .then(r => console.log(r))
   }, [])
 
-
   return (
     <>
       <Header />
       <main>
+        <button onClick={postare}>test</button>
         <Button text={'my button'} />
         <section>
           {projects.map(e => <ProfileCard key={e._id} name={e.name} job={e.description} imageUrl={e.image} />)}
