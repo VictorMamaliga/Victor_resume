498febf3edf7aeb9e8f4d8de3c92fa76875e561f
Victor Mamaliga <m.viktor24.vm@gmail.com>
2023-06-21 00:15:34 +0100

    evirca-button

--- personalSite/frontend/src/components/atoms/Button.jsx
+++ personalSite/frontend/src/components/atoms/Button.jsx
@@ -1,13 +1,16 @@
 import styles from './button.module.scss';
 
 const Button = ({ text }) => {
-    return (
-      <button className={styles.button}>
-        <div className={styles.buttonSlider}>
-          <h3>{text} </h3>
-        </div>
-      </button>
-    );
-  };
-  
-  export default Button;
+  return (
+    <button className={styles.button}>
+      <div id='rssBlock'>
+        <p className={styles.cnnContents}>
+          <span className={styles.marqueeStyle}>continuous infinite text loop 1 continuous infinite text loop 2 </span>
+          <span className={styles.marqueeStyle2}>continuous infinite text loop 3 continuous infinite text loop 4</span>
+        </p>
+      </div>
+    </button>
+  );
+};
+
+export default Button;
