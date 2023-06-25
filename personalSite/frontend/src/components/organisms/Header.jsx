b50b5b3425347aff448a5e925d0d27df31ba324a
George <g3orrg301@gmail.com>
2023-06-25 15:03:12 +0300

    Quick styling for header

--- personalSite/frontend/src/components/organisms/Header.jsx
+++ personalSite/frontend/src/components/organisms/Header.jsx
@@ -3,19 +3,17 @@ import styles from './header.module.scss';
 export default function Header() {
     return (
         <header className={styles.header}>
-            <nav>
-                <div className={styles.headerName}>
-                    <a>Gutan Bregovih</a>
-                    <span>INTERPRET PRE/POST INTERBELIC TALENT</span>
+            <div className='header-top-area'>
+                <div className={styles.headerContainer}>
+                    <a href="" className="name">George</a>
+                    <div className={styles.headerHamburger}>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
                 </div>
-                <div>
-                    <ul>
-                        <li><a href='#first'>accenti,</a></li>
-                        <li><a href='#second'>capatos,</a></li>
-                        <li><a href='#third'>mustati</a></li>
-                    </ul>
-                </div>
-            </nav>
+            </div>
+            <div className="welcome-image-area"></div>
         </header>
     );
 }
