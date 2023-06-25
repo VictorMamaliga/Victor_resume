ff0c03d39883c377ede50c94baf4c155f3568f4c
George <g3orrg301@gmail.com>
2023-06-25 22:59:10 +0300

    Started work on sidebar

--- personalSite/frontend/src/components/organisms/Sidebar.jsx
+++ personalSite/frontend/src/components/organisms/Sidebar.jsx
@@ -2,6 +2,15 @@ import styles from './sidebar.module.scss';
 
 export default function Sidebar() {
     return (
-        <div className={styles.sidebar}>Sidebar</div>
+        <div className={styles.sidebar}>
+            <span>X</span>
+            <ul>
+                <li><a href="">Mancare</a></li>
+                <li><a href="">Bautura</a></li>
+                <li><a href="">Tigari</a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+            </ul>
+        </div>
     );
 }
