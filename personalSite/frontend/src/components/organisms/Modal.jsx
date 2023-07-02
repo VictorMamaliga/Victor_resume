e84f36118b3c737b79bfb6a91b425a7472718809
George <g3orrg301@gmail.com>
2023-07-02 22:03:42 +0300

    Styling improvements

--- personalSite/frontend/src/components/organisms/Modal.jsx
+++ personalSite/frontend/src/components/organisms/Modal.jsx
@@ -5,8 +5,8 @@ const Modal = ({ isOpen, apiResponseStatus, children, onToggleModal }) => {
         <div className={styles.modal}>
             <div className={styles.modalWrapper}>
                 <button className={styles.modalClose} onClick={() => onToggleModal()}>X</button>
-                {apiResponseStatus == 201 && (<h4>Success</h4>)}
-                {apiResponseStatus == 404 && (<h4>Error</h4>)}
+                {apiResponseStatus == 201 && (<h4 className='api-response-status'>Success</h4>)}
+                {apiResponseStatus == 404 && (<h4 className='api-response-status'>Error</h4>)}
                 {!apiResponseStatus && (children)}
             </div>
         </div>
