622c4f1190b179e81371caa536c9a2c33a370eea
George <g3orrg301@gmail.com>
2023-07-04 23:17:31 +0300

    Deployed client to firebase

--- personalSite/nestjs-backend/firebase/config.ts
+++ personalSite/nestjs-backend/firebase/config.ts
@@ -2,7 +2,7 @@ const admin = require('firebase-admin');
 
 const firebaseConfig = {
   projectId: process.env.FIREBASE_PROJECT_ID,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  privateKey: process.env.FIREBASE_PRIVATE_KEY,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
 };
 
